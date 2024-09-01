5348c181fa10787027e10ec967b4df10ddf85a41
kartikey pandey <kartikey224252@gmail.com>
2024-09-01 16:05:03 +0530

    chore: Add solution for setMatrixZeroes problem

---
+++ Arrays/setMatrixZeroes.cpp
@@ -0,0 +1,4 @@
+// question : 73 ===> in brute method when in coloumn initialse everything except zero to any other number except 0 and one lets say u initialised it with an -1 than in next iteration u again initialse everything that is -1 to zero and u get your desired output  https://leetcode.com/problems/set-matrix-zeroes/description/ T.C O(n3)
+
+// better solution
+
