227ff97635aa9a92c95ed551d319e80027eb04f1
kartikey pandey <kartikey224252@gmail.com>
2024-09-24 20:09:25 +0530

    Add mergeSort.cpp and insertionSort.cpp

---
+++ sorting/insertionSort.cpp
@@ -0,0 +1,31 @@
+/*
+? bubbleSort: adjacent swapping will be done
+*/
+#include<bits/stdc++.h>
+using namespace std;
+void InsertionSort(int arr[],int size){
+    for(int i=0;i<=size;i++){
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j]){
+          swap(arr[j-1],arr[j]);
+          j--;
+        }
+    }
+}
+int main() {
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+    int arr[size];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+    InsertionSort(arr, size);
+    cout << "Sorted array is: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
+// worst case => O(n^2)
