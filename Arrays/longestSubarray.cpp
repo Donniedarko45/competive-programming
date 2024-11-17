4abd426bd06efa4752e2fe948110c89838111f9e
Kartikey Pandey <kartikey224252@gmail.com>
2024-11-18 00:10:31 +0530

    started B.S

--- Arrays/longestSubarray.cpp
+++ Arrays/longestSubarray.cpp
@@ -1,30 +1,31 @@
 /*Longest subarray with sum k*/
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 // brute
-int longestSubarray(int arr[],int n,int k){
-    int len=0,j;
-    for(int i=0;i<n;i++){
-        int sum=0;
-        for(j=i;j<n;j++){
-            sum+=arr[j];
-        }
-        if(sum==k){
-            len=max(len,j-i+1);
-        }
+int longestSubarray(int arr[], int n, int k) {
+  int len = 0, j;
+  for (int i = 0; i < n; i++) {
+    int sum = 0;
+    for (j = i; j < n; j++) {
+      sum += arr[j];
     }
-    cout<<"length of max subarray with sum = k is : "<<len;
+    if (sum == k) {
+      len = max(len, j - i + 1);
+    }
+  }
+  cout << "length of max subarray with sum = k is : " << len;
+  return len;
 }
-int main(){
+int main() {
   int n;
-  cout<<"enter the size of array: ";
-  cin>>n;
+  cout << "enter the size of array: ";
+  cin >> n;
   int arr[n];
-  for(int i=0;i<n;i++){
-    cin>>arr[n];
+  for (int i = 0; i < n; i++) {
+    cin >> arr[n];
   }
   int k;
-  cout<<"enter the summation of max-subArray: ";
-  cin>>k;
-  longestSubarray(arr,n,k);
+  cout << "enter the summation of max-subArray: ";
+  cin >> k;
+  longestSubarray(arr, n, k);
 }
---
+++ BinarySearch/BinarySearch.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int IndexOfElement(int arr[], int size, int target) {
+  int low = 0;
+  int high = size - 1;
+  int mid;
+  while (low <= high) {
+    mid = (low + high) / 2;
+    if (arr[mid] == target)
+      return mid;
+    else if (arr[mid] > target)
+      high = mid - 1;
+    else
+      low = mid + 1;
+  }
+  return -1;
+}
+
+int IndexOfElementRecursive(int arr[], int size, int target, int low,
+                            int high) {
+  if (low > high)
+    return -1;
+  int mid = (low + high) / 2;
+  if (arr[mid] == target)
+    return mid;
+  else if (target > arr[mid])
+    return IndexOfElementRecursive(arr, size, 8, mid + 1, high);
+  return IndexOfElementRecursive(arr, size, 8, low, mid - 1);
+}
+
+int main() {
+  int target = 16;
+  int arr[8] = {3, 4, 6, 7, 9, 12, 16, 17};
+  cout << IndexOfElement(arr, 8, target);
+}
